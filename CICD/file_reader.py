55d020ccde15f18427322161fc9e3fbc953ed04d
Felicio de Mattos Araujo <ygor.felicio@lexisnexisrisk.com>
2022-10-20 11:59:08 -0300

    Updated

---
+++ CICD/file_reader.py
@@ -0,0 +1,9 @@
+# ============================================== MAIN FUNCTION
+if __name__ == '__main__':
+    print("\n\n")
+    print("***********************************************************")
+    print("Reading the content of the file")
+    print("***********************************************************")
+
+    file = open('C:/Users/feliyg01/test.txt')
+    file.readline()
